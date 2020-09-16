1ab1551326a194ad305c10e43a6b254423048a68
haytham taha alsharany <talsharany@gmail.com>
2020-09-16 15:44:42 +0300

    first

---
+++ app/src/main/java/com/alsharany/android_challenge_1_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alsharany.android_challenge_1_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
